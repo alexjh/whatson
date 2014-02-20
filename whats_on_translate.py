0fed99118a5bb27bc53f0471709bf999a72337a0
Alex Harford <harford@gmail.com>
2014-02-20 10:58:23 -0800

    whats_on_translate.py: Split YAML overrides and translations
    Overrides should be used before any remote queries as they are known fixups.
    Translations are guesses and are tried after the initial query does not contain enough information

--- whats_on_translate.py
+++ whats_on_translate.py
@@ -1,18 +1,52 @@
 #!/usr/bin/python
 
 
+from __future__ import print_function
+import yaml
+
+VERBOSE = False
+
 # Translations:
 #
-# * TODO: YAML based
+# * YAML based takes precedence
 # * Artist: Remove anything past " feat " or " feat." or " w/" or " &"
 # * Song: Remove parens
 # * Song: Remove anything past "/"
 
 EXAMPLE_ARTISTS = ['Jimmy Buffett W/Clint Black, Kenny Chesney, Alan Jackson, Toby Keith & Geor',
                    'Dude & Foo feat dude1',
+                   'Guess Who',
                   ]
 
-EXAMPLE_SONGS = []
+EXAMPLE_SONGS = [('Trews', 'Power Of Positive Drinking'),
+                 ('Prince', '- 1999 -'),
+                 ('Trooper', 'The Boys In The Bright White Sports Cars')]
+
+TRANSLATIONS = {}
+
+def load_yaml():
+    try:
+        translations_yaml = open('translations.yaml')
+        translations = yaml.load(translations_yaml)
+    except IOError:
+        translations = {}
+
+    return translations
+
+def override_artist( artist ):
+    """Overrides the artist name
+
+    Arguments:
+
+    artist -- string containing name
+
+    """
+
+    if artist in TRANSLATIONS['artists'].keys():
+        if VERBOSE:
+            print("Translating", artist, "to", TRANSLATIONS['artists'][artist])
+        return TRANSLATIONS['artists'][artist]
+    return None
 
 def translate_artist( artist ):
     """Converts artist name into something that last.fm / Musicbrainz 
@@ -24,8 +58,8 @@ def translate_artist( artist ):
 
     Returns the translated name"""
 
+
     artist_lower = artist.lower()
-    print artist_lower
 
     shortest_pos = len(artist_lower)
 
@@ -39,8 +73,26 @@ def translate_artist( artist ):
 
     return artist[0:shortest_pos]
 
+def override_song( song ):
+    """Overrides the song title from YAML config
+
+    Arguments:
+
+    song -- tuple of (artist, song)
+
+    Returns new song title or None
+    
+    """
+
+    artist = translate_artist(song[0])
+
+    if artist in TRANSLATIONS['songs'].keys():
+        if song[1] in TRANSLATIONS['songs'][artist].keys():
+            return str(TRANSLATIONS['songs'][artist][song[1]])
+    return None
+
 def translate_song( song ):
-    """Converts song title into something that last.fm / Musicbrainz 
+    """Converts artist and song title into something that last.fm / Musicbrainz 
     understands
 
     Arguments:
@@ -49,8 +101,8 @@ def translate_song( song ):
 
     Returns the translated name"""
 
-    song_lower = song.lower()
-    print song_lower
+
+    song_lower = song[1].lower()
 
     shortest_pos = len(song_lower)
 
@@ -60,15 +112,25 @@ def translate_song( song ):
         if pos > 0 and pos < shortest_pos:
             shortest_pos = pos
 
-    return song[0:shortest_pos]
+    return song[1][0:shortest_pos]
 
 
 def main():
     for artist in EXAMPLE_ARTISTS:
-        print translate_artist( artist )
+        artist_or = override_artist( artist )
+        if artist_or:
+            print(artist_or)
+        else:
+            print(translate_artist( artist ))
 
     for song in EXAMPLE_SONGS:
-        print translate_song( song )
+        song_or = override_song( song )
+        if song_or:
+            print(song_or)
+        else:
+            print(translate_song( song ))
+
+TRANSLATIONS = load_yaml()
 
 if __name__ == '__main__':
     main()
