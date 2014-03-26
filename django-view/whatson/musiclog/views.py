b24cbc146af813e969fec9b26767571926f733e5
Alex Harford <harford@gmail.com>
2014-03-25 18:02:43 -0700

    Refactor some copypasta shared across the various functions

--- django-view/whatson/musiclog/views.py
+++ django-view/whatson/musiclog/views.py
@@ -5,18 +5,51 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.db.models import Count
 
-from musiclog.models import Airplay, Artist, Track, Release, Station
+from musiclog.models import Airplay, Artist, Track, Station
 
-def gen_colour_list( length ):
+def gen_colour_list(length):
+    """Return a list of colours of size 'length'"""
     colour = []
     colour_seed = 0xF7464A
 
-    for i in range(0,length):
-        colour.append( "#%06X" % colour_seed)
+    for _ in range(0, length):
+        colour.append("#%06X" % colour_seed)
         colour_seed += 0xB46090
         colour_seed %= 0xFFFFFF
 
     return colour
+
+def gen_graph_array(variable, graph_data):
+    """Generates a javascript array to be used by the doughnut graph"""
+    array_text = 'var %s = [\n' % variable
+
+    for i in graph_data:
+        array_text += ('  {\n')
+        for k, value in i.iteritems():
+            if k == 'color':
+                array_text += '    ' + k + ": " + '"' + value + '",\n'
+            else:
+                array_text += '    ' + k + ": " + str(value) + ',\n'
+        array_text += '  },\n'
+
+    array_text += '];\n'
+
+    return array_text
+
+def gen_doughnut_data(value_dict, value_key, var_name):
+    """Generates a doughnut graph javascript variable string
+    to be used by the JS graphing library"""
+    colour = gen_colour_list(len(value_dict))
+
+    data = []
+    for colour_idx, value in enumerate(value_dict):
+        data.append({
+            'color': colour[colour_idx],
+            'value': value[value_key],
+            })
+
+    return gen_graph_array(var_name, data), colour
+
 
 def index(request):
     """Main page of the musiclog app"""
@@ -28,9 +61,63 @@ def index(request):
     return HttpResponse(template.render(context))
 
 
+def airplay_all(request):
+    """Airplay summary page of the musiclog app"""
+
+    recent_songs = Airplay.objects.order_by('-timestamp')
+
+    template = loader.get_template('musiclog/airplay_all.html')
+    context = RequestContext(request, {
+                'recent_songs': recent_songs[:20],
+                    })
+    return HttpResponse(template.render(context))
 
 
-def airplay(request, airplay_id = 0):
+def track_all(request):
+    """track summary page of the musiclog app"""
+
+    recent_songs = Airplay.objects.order_by('-timestamp')
+
+    template = loader.get_template('musiclog/track_all.html')
+    context = RequestContext(request, {
+                'recent_songs': recent_songs[:20],
+                    })
+    return HttpResponse(template.render(context))
+
+
+def release_all(request):
+    """release summary page of the musiclog app"""
+
+    template = loader.get_template('musiclog/release_all.html')
+    context = RequestContext(request, {
+                    })
+    return HttpResponse(template.render(context))
+
+
+def artist_all(request):
+    """artist summary page of the musiclog app"""
+
+    recent_songs = Airplay.objects.order_by('-timestamp')
+    template = loader.get_template('musiclog/artist_all.html')
+    context = RequestContext(request, {
+                'recent_songs': recent_songs[:20],
+                    })
+    return HttpResponse(template.render(context))
+
+
+def station_all(request):
+    """station summary page of the musiclog app"""
+
+    stations = Station.objects.all().order_by('name')
+
+    template = loader.get_template('musiclog/station_all.html')
+    context = RequestContext(request, {
+                'stations': stations,
+                    })
+    return HttpResponse(template.render(context))
+
+
+def airplay(request):
     """Airplay page of the musiclog app"""
     template = loader.get_template('musiclog/airplay.html')
     context = RequestContext(request, {
@@ -38,67 +125,57 @@ def airplay(request, airplay_id = 0):
     return HttpResponse(template.render(context))
 
 
-
-
-def track(request, track_id = 0):
+def track(request, track_id=0):
     """Track page of the musiclog app"""
 
     track_info = Track.objects.get(pk=track_id)
-    newest_play = Airplay.objects.filter( track__pk = track_id ).\
+    newest_play = Airplay.objects.filter(track__pk=track_id).\
                       order_by('-timestamp')
-    first_play = Airplay.objects.filter( track__pk = track_id ).\
+    first_play = Airplay.objects.filter(track__pk=track_id).\
                       order_by('timestamp')[0]
 
     # station_plays is the Airplays of the track, grouped by station
     # and ordered by station
-    station_plays = Airplay.objects.filter( track__pk = track_id ) \
-                        .values( 'station__name', 'station__id') \
+
+    num_stations = 20
+    station_plays = Airplay.objects.filter(track__pk=track_id) \
+                        .values('station__name', 'station__id') \
                         .annotate(Count('station__id'))\
-                        .order_by('-station__id__count')
+                        .order_by('-station__id__count')[:num_stations]
+
+    # Get the absolute urls for the stations
+    station_urls = []
+    for station_query in station_plays:
+        station_obj = Station.objects.get(pk=station_query['station__id'])
+        station_urls.append(station_obj.get_absolute_url())
 
 
-    data = []
-
-    colour = gen_colour_list( len(station_plays) )
-
-    for index, station in enumerate(station_plays):
-        temp_dict = {}
-        temp_dict['color'] = colour[index]
-        temp_dict['value'] = station['station__id__count']
-        data.append(temp_dict)
-
-    station_doughnut = "var station_doughnut = "
-    station_doughnut += '[\n'
-
-    for i in data:
-        station_doughnut += ('  {\n')
-        for k,v in i.iteritems():
-            if k == 'color':
-                station_doughnut += '    ' + k + ": " + '"' + v + '",\n'
-            else:
-                station_doughnut += '    ' + k + ": " + str(v) + ',\n'
-        station_doughnut += '  },\n'
+    # Generate the javascript variable used for the doughnut graph
+    doughnut_data, colour = gen_doughnut_data(station_plays,
+                                      'station__id__count',
+                                      'station_doughnut')
 
 
-    station_doughnut += '];\n'
-
+    # Set up the Django template and context
     template = loader.get_template('musiclog/track.html')
     context = RequestContext(request, {
-                'track_info':  track_info,
-                'newest_play': newest_play[0],
-                'first_play':  first_play,
-                'track_plays':  newest_play[:10],
-                'station_plays':  zip(station_plays[:20], colour),
-                'station_doughnut': station_doughnut,
-                'table_colour': colour,
-                    })
+        'track_info':  track_info,
+        'newest_play': newest_play[0],
+        'first_play':  first_play,
+        'track_plays':  newest_play[:10],
+        'station_plays':  zip(station_plays,
+            colour,
+            station_urls,
+            ),
+        'station_doughnut': doughnut_data,
+        })
     return HttpResponse(template.render(context))
 
 
 
 
 
-def release(request, release_id = 0):
+def release(request):
     """Release page of the musiclog app"""
     template = loader.get_template('musiclog/release.html')
     context = RequestContext(request, {
@@ -106,105 +183,65 @@ def release(request, release_id = 0):
     return HttpResponse(template.render(context))
 
 
-# JSON views for:
-#
-# Artist view:
-#
-# * airplay counts by artist
-# * artist counts by station
 
-def artist(request, artist_id = 0):
+def artist(request, artist_id=0):
     """Artist page of the musiclog app"""
 
-    # Variables for template:
-    artist_info = Artist.objects.get(pk=artist_id)
-
     # List of all tracks belonging to this artist
-    artist_tracks = Track.objects.filter( artist__pk = artist_id )
+    artist_tracks = Track.objects.filter(artist__pk=artist_id)
 
-    latest_airplays = Airplay.objects.filter( track__in = artist_tracks ) \
+    latest_airplays = Airplay.objects.filter(track__in=artist_tracks) \
                          .order_by('-timestamp')[:10]
 
-    airplay_counts = Airplay.objects.filter( track__in = artist_tracks )\
-                        .values( 'track__title', 'track__id') \
+    airplay_counts = Airplay.objects.filter(track__in=artist_tracks)\
+                        .values('track__title', 'track__id') \
                         .annotate(Count('track__id'))\
-                        .order_by('-track__id__count')
+                        .order_by('-track__id__count')[:10]
 
+    airplay_urls = []
+    for airplay_query in airplay_counts:
+        airplay_obj = Track.objects.get(pk=airplay_query['track__id'])
+        airplay_urls.append(airplay_obj.get_absolute_url())
 
-    # TODO Generate a colour list of len(airplay_counts)
-    # TODO Generate the graph data via helper function
-    # TODO Display a legend with an icon for the colour to track mapping
+    doughnut_data, colour = gen_doughnut_data(airplay_counts,
+            'track__id__count',
+            'doughnut_data',
+            )
 
-    data = []
-    colour = gen_colour_list( len(airplay_counts) )
-    for index, airplay in enumerate(airplay_counts):
-        temp_dict = {}
-        temp_dict['color'] = colour[index]
-        temp_dict['value'] = airplay['track__id__count']
-        data.append(temp_dict)
-
-    doughnut_data = "var doughnut_data = "
-    doughnut_data += '[\n'
-
-    for i in data:
-        doughnut_data += ('  {\n')
-        for k,v in i.iteritems():
-            if k == 'color':
-                doughnut_data += '    ' + k + ": " + '"' + v + '",\n'
-            else:
-                doughnut_data += '    ' + k + ": " + str(v) + ',\n'
-        doughnut_data += '  },\n'
-
-
-    doughnut_data += '];\n'
-
-
-    # TODO station_doughnut
-    #
     # Use tracks by artist to find Airplays by artist, grouped by station
-    station_counts = Airplay.objects.filter( track__in = artist_tracks )\
-                        .values( 'station__name', 'station__id') \
+    station_counts = Airplay.objects.filter(track__in=artist_tracks)\
+                        .values('station__name', 'station__id') \
                         .annotate(Count('station__id'))\
-                        .order_by('-station__id__count')
+                        .order_by('-station__id__count')[:20]
 
-    data = []
-    colour = gen_colour_list( len(station_counts) )
+    station_urls = []
+    for station_query in station_counts:
+        station_obj = Station.objects.get(pk=station_query['station__id'])
+        station_urls.append(station_obj.get_absolute_url())
 
-    for index, station in enumerate(station_counts):
-        temp_dict = {}
-        temp_dict['color'] = colour[index]
-        temp_dict['value'] = station['station__id__count']
-        data.append(temp_dict)
+    doughnut_data2, colour2 = gen_doughnut_data(station_counts,
+            'station__id__count',
+            'station_doughnut',
+            )
 
-    station_doughnut = "var station_doughnut = "
-    station_doughnut += '[\n'
-
-    # TODO FIXME Move this to a function
-    for i in data:
-        station_doughnut += ('  {\n')
-        for k,v in i.iteritems():
-            if k == 'color':
-                station_doughnut += '    ' + k + ": " + '"' + v + '",\n'
-            else:
-                station_doughnut += '    ' + k + ": " + str(v) + ',\n'
-        station_doughnut += '  },\n'
-
-
-    station_doughnut += '];\n'
-
-    # Template File
     template = loader.get_template('musiclog/artist.html')
-
-    # Mapping of variables to template variables
     context = RequestContext(request, {
-                'latest_airplays': latest_airplays,
-                'artist_tracks': artist_tracks,
-                'artist_info': artist_info,
-                'airplay_popular': zip(airplay_counts[:10], colour),
-                'doughnut_data': doughnut_data,
-                'station_plays': zip(station_counts[:20], colour),
-                'station_doughnut': station_doughnut,
-                    })
+        'latest_airplays': latest_airplays,
+        'artist_tracks': artist_tracks,
+        'artist_info': Artist.objects.get(pk=artist_id),
+        'airplay_popular': zip(
+            airplay_counts,
+            colour,
+            airplay_urls,
+            ),
+        'doughnut_data': doughnut_data,
+        'station_plays': zip(
+            station_counts,
+            colour2,
+            station_urls,
+            ),
+        'station_doughnut': doughnut_data2,
+        })
 
     return HttpResponse(template.render(context))
 
@@ -212,63 +249,49 @@ def artist(request, artist_id = 0):
 
 
 
-
-
-
-def station(request, station_id = 0):
+def station(request, station_id=0):
     """Station page of the musiclog app"""
 
     station_info = Station.objects.get(pk=station_id)
 
     # List of all tracks belonging to this station
-    recent_songs = Airplay.objects.filter( station__pk = station_id ) \
+    recent_songs = Airplay.objects.filter(station__pk=station_id) \
                         .order_by('-timestamp')
-    
-    popular_artists = Airplay.objects.filter( station__pk = station_id ) \
-                        .values( 'track__artist__id', 'track__artist__name') \
-                        .annotate(Count('track__artist__id'))\
-                        .order_by('-track__artist__id__count')
+
 
     artists_num = 15
-
-    # station_doughnut
-    #
-    # Find Airplays by station, grouped by artist
-    station_counts = Airplay.objects.filter( station__pk = station_id )\
-                        .values( 'track__artist__name', 'track__artist__id') \
+    popular_artists = Airplay.objects.filter(station__pk=station_id) \
+                        .values('track__artist__id', 'track__artist__name') \
                         .annotate(Count('track__artist__id'))\
                         .order_by('-track__artist__id__count')[:artists_num]
 
-    data = []
-    colour = gen_colour_list( len(station_counts) )
+    artist_urls = []
+    for artist_query in popular_artists:
+        artist_obj = Artist.objects.get(pk=artist_query['track__artist__id'])
+        artist_urls.append(artist_obj.get_absolute_url())
 
-    for index, artist in enumerate(station_counts):
-        temp_dict = {}
-        temp_dict['color'] = colour[index]
-        temp_dict['value'] = artist['track__artist__id__count']
-        data.append(temp_dict)
+    # Find Airplays by station, grouped by artist
+    station_counts = Airplay.objects.filter(station__pk=station_id)\
+                        .values('track__artist__name', 'track__artist__id') \
+                        .annotate(Count('track__artist__id'))\
+                        .order_by('-track__artist__id__count')[:artists_num]
 
-    artists_doughnut = "var artists_doughnut = "
-    artists_doughnut += '[\n'
-
-    for i in data:
-        artists_doughnut += ('  {\n')
-        for k,v in i.iteritems():
-            if k == 'color':
-                artists_doughnut += '    ' + k + ": " + '"' + v + '",\n'
-            else:
-                artists_doughnut += '    ' + k + ": " + str(v) + ',\n'
-        artists_doughnut += '  },\n'
-
-
-    artists_doughnut += '];\n'
+    doughnut_data, colour = gen_doughnut_data(
+            station_counts,
+            'track__artist__id__count',
+            'artists_doughnut',
+            )
 
     template = loader.get_template('musiclog/station.html')
     context = RequestContext(request, {
-                'station_info': station_info,
-                'recent_songs': recent_songs[:10],
-                'popular_artists': zip(popular_artists[:artists_num], colour),
-                'artists_doughnut': artists_doughnut,
-                    })
+        'station_info': station_info,
+        'recent_songs': recent_songs[:10],
+        'popular_artists': zip(
+            popular_artists,
+            colour,
+            artist_urls,
+            ),
+        'artists_doughnut': doughnut_data,
+        })
     return HttpResponse(template.render(context))
 
