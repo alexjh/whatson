5efbeb100118bf06cf7d3415f0e9b77e59cb4a2e
Alex Harford <harford@gmail.com>
2014-03-25 17:58:58 -0700

    Use generic views for urls without parameters

--- django-view/whatson/musiclog/urls.py
+++ django-view/whatson/musiclog/urls.py
@@ -6,11 +6,11 @@ urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
 
         # General view
-        url(r'airplay/$', views.airplay, name='airplay'),
-        url(r'track/$',   views.track,   name='track'),
-        url(r'release/$', views.release, name='release'),
-        url(r'artist/$',  views.artist,  name='artist'),
-        url(r'station/$', views.station, name='station'),
+        url(r'airplay/$', views.airplay_all, name='airplay_all'),
+        url(r'track/$',   views.track_all,   name='track_all'),
+        url(r'release/$', views.release_all, name='release_all'),
+        url(r'artist/$',  views.artist_all,  name='artist_all'),
+        url(r'station/$', views.station_all, name='station_all'),
 
         # Pages for specific items
         url(r'airplay/(?P<airplay_id>\d+)/$', views.airplay, name='airplay'),
